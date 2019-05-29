a6585db3fbb02f013fea8d4d4ad54ccd68c3b0fa
suhail.bulla <suhail.bulla@IRIS-CSG-868.irissoftware.com>
2019-05-29 15:30:13 +0530

    Initial commit

---
+++ java-practice/src/practice/sorting/map/Frequency.java
@@ -0,0 +1,8 @@
+package practice.sorting.map;
+
+public class Frequency {
+
+	public static void main(String[] args) {
+		int[] a = {1, 2, 1, 3, 1, 4, 2, 3};
+	}
+}
---
+++ java-practice/src/practice/sorting/map/MapSort.java
@@ -0,0 +1,17 @@
+package practice.sorting.map;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapSort {
+
+	public static void main(String[] args) {
+		Map<String, Integer> map = new HashMap<>();
+		map.put("C", 3);
+		map.put("D", 4);
+		map.put("E", 5);
+		map.put("B", 2);
+		map.put("A", 1);
+		System.out.println(map);
+	}
+}
